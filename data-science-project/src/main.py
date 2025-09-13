193227d7e24c3c4d493a55ce2f02b2847fef73c5
komarovala <starkcom8@mail.ru>
2025-09-13 17:17:37 +0300

    fisrt

--- data-science-project/src/main.py
+++ data-science-project/src/main.py
@@ -1,14 +1,14 @@
 # main.py
 
 import pandas as pd
-from utils import load_data, process_data, train_model
+from utils import load_data, preprocess_data, train_model
 
 def main():
     # Load raw data
     raw_data = load_data('data/raw/data.csv')
     
     # Process the data
-    processed_data = process_data(raw_data)
+    processed_data = preprocess_data(raw_data)
     
     # Train the model
     model = train_model(processed_data)
